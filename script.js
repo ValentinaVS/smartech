5b85a9c8178c94a3121b40ee537f32a8ade012ee
ValentinaVS <37575183+ValentinaVS@users.noreply.github.com>
2019-07-12 02:43:04 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var elem = document.getElementById ('t');
+		t.onclick = function()
+		{
+			if (b.style.backgroundColor) b.style.backgroundColor = "";
+			else b.style.backgroundColor = "#c00";
+		}
